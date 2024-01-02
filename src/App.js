700beecbe39e16d49770a6735d616855ca2ae67e
sidsrivastavasks <sidsrivastavasks@gmail.com>
2024-01-02 23:50:20 +0530

    checkpoint 3 done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Header from "./Component/Header/Header.js";
 import Body from "./Component/Body/List.js";
 
 export default function App() {
-    const listData = [];
+    const listData = [
+        "Read SpringBoot",
+        "Complete assignments",
+        "Prepare breakfast",
+        "Sleep for 2 hours",
+        "Take a shower",
+    ];
     return (
         <div className="App">
             <Header />
